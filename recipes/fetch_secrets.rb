b11ab39f1ed1791e00688850d927df9a10a18f60
Alexander Birk <birk@pingworks.de>
2015-11-16 21:18:17 +0100

    set workshop box name very early

--- recipes/fetch_secrets.rb
+++ recipes/fetch_secrets.rb
@@ -12,4 +12,15 @@ cookbook_file '/tmp/id_rsa' do
   mode 00600
 end
 
+directory '/etc/workshopbox' do
+  owner 'root'
+  group 'root'
+  mode 00755
+  recursive true
+  action :create
+end
+
+template '/etc/workshopbox/workshop_name'
+cookbook_file '/etc/workshopbox/workshop_domainkey'
+
 execute 'ssc-fetch-secrets'
