f008d911528b653df4faa032af38d019d99b48d8
Alexander Birk <birk@pingworks.de>
2016-10-19 22:56:09 +0200

    print login list

---
+++ recipes/print_loginlist.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: secret_service
+#
+# Copyright (C) 2015 Alexander Birk
+#
+# Licensed under the Apache License, Version 2.0
+#
+
+package 'pwgen'
+
+cookbook_file '/tmp/id_rsa' do
+  owner 'root'
+  group 'root'
+  mode 00600
+end
+
+template '/opt/secret_service/bin/ssc-print-loginlist.sh' do
+  owner 'root'
+  group 'root'
+  mode 00755
+end
+
+link '/usr/local/bin/ssc-print-loginlist' do
+  to '/opt/secret_service/bin/ssc-print-loginlist.sh'
+end
+
+exec 'opt/secret_service/bin/ssc-print-loginlist.sh'
