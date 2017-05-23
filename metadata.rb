bc79e4393be133606ffaaaf89c537d58e81233da
Alexander Birk <birk@pingworks.de>
2017-05-23 11:20:28 +0200

    no random passwords

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'birk@pingworks.de'
 license 'Apache 2.0'
 description 'Installs/Configures secret_service'
 long_description 'Installs/Configures secret_service'
-version '0.2.13'
+version '0.2.14'
 
 depends 'apt', '~> 5.1.0'
